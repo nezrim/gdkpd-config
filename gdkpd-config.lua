2c18f853fc2225e1251242f30a6523099de7d867
Kiri Dániel <37482263+nezrim@users.noreply.github.com>
2022-06-08 14:59:37 +0200

    Create gdkpd-config.lua

---
+++ gdkpd-config.lua
@@ -0,0 +1,602 @@
+["Default"] = {
+			["balancepoint"] = {
+				["y"] = 3.056400529289879,
+				["relative"] = "TOPLEFT",
+				["point"] = "TOPLEFT",
+				["x"] = 172.8739777048025,
+			},
+			["auctionTimer"] = 10,
+			["movable"] = false,
+			["anchorBalance"] = true,
+			["playerbalancepoint"] = {
+				["y"] = 4.000030040740967,
+				["relative"] = "TOP",
+				["point"] = "TOP",
+				["x"] = 25.99986839294434,
+			},
+			["customItemSettings"] = {
+				[30863] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[30879] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[30883] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[30895] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[30903] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[30911] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32234] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32238] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[32497] = {
+					["minIncrement"] = 100,
+					["minBid"] = 3000,
+				},
+				[32501] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32505] = {
+					["minIncrement"] = 100,
+					["minBid"] = 3500,
+				},
+				[32254] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32258] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32262] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32266] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32525] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32278] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32326] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32330] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32593] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32342] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32346] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32350] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32609] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32362] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32366] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32370] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[31099] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[31103] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[30864] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[30872] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[30884] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[30888] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[30896] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[30900] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[30908] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[30912] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[30916] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32235] = {
+					["minIncrement"] = 100,
+					["minBid"] = 4000,
+				},
+				[32239] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32243] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32247] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32255] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32263] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32271] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32279] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32323] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32331] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32335] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32339] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32343] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[32347] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32363] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32367] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[31096] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[31100] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[30861] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[30865] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[30881] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[30889] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[30893] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[30897] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[30901] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[30905] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[30909] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[30913] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32483] = {
+					["minIncrement"] = 100,
+					["minBid"] = 4000,
+				},
+				[32232] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32236] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32248] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32252] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[32256] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32260] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32268] = {
+					["minIncrement"] = 50,
+					["minBid"] = 100,
+				},
+				[32527] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32276] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32280] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32324] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32328] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32332] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32336] = {
+					["minIncrement"] = 100,
+					["minBid"] = 3000,
+				},
+				[32344] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[32352] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[31089] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32368] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[31097] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[31101] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[30862] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[30870] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[30874] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[30882] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32837] = {
+					["minIncrement"] = 500,
+					["minBid"] = 30000,
+				},
+				[30898] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[30902] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[30906] = {
+					["minIncrement"] = 100,
+					["minBid"] = 3000,
+				},
+				[30910] = {
+					["minIncrement"] = 100,
+					["minBid"] = 3000,
+				},
+				[30914] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[30918] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[31095] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32237] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32496] = {
+					["minIncrement"] = 100,
+					["minBid"] = 4000,
+				},
+				[32245] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[31092] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32373] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32512] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[32261] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32265] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32524] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2500,
+				},
+				[32528] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[31094] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[32946] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[34010] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[34009] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32838] = {
+					["minIncrement"] = 500,
+					["minBid"] = 25000,
+				},
+				[34012] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32590] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32526] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32591] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[34011] = {
+					["minIncrement"] = 50,
+					["minBid"] = 600,
+				},
+				[32374] = {
+					["minIncrement"] = 100,
+					["minBid"] = 5000,
+				},
+				[32521] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32375] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2500,
+				},
+				[32500] = {
+					["minIncrement"] = 100,
+					["minBid"] = 4000,
+				},
+				[32333] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32337] = {
+					["minIncrement"] = 50,
+					["minBid"] = 800,
+				},
+				[32341] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2500,
+				},
+				[32345] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32349] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[32376] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+				[31091] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[31093] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[31090] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32369] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2500,
+				},
+				[31098] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1500,
+				},
+				[31102] = {
+					["minIncrement"] = 50,
+					["minBid"] = 2000,
+				},
+				[32510] = {
+					["minIncrement"] = 50,
+					["minBid"] = 1000,
+				},
+			},
+			["itemLevelPricing"] = {
+				{
+					["min"] = 0,
+					["mininc"] = 10,
+					["max"] = 101,
+					["minbid"] = 50,
+				}, -- [1]
+				{
+					["min"] = 102,
+					["mininc"] = 50,
+					["max"] = 170,
+					["minbid"] = 300,
+				}, -- [2]
+			},
+			["point"] = {
+				["y"] = -168.3966827392578,
+				["x"] = 349.809814453125,
+				["point"] = "TOP",
+				["relative"] = "TOP",
+			},
+			["auctionTimerRefresh"] = 10,
+			["hideCombat"] = {
+				["status"] = true,
+				["history"] = true,
+				["vercheck"] = true,
+			},
+			["linkBalancePot"] = true,
+			["hide"] = false,
+			["awardToML"] = true,
+			["startBid"] = 100,
+			["confirmMail"] = true,
+			["showAuctionDurationTimerText"] = true,
+			["announceBidRaidWarning"] = true,
+			["statuspoint"] = {
+				["y"] = -66.10133361816406,
+				["relative"] = "TOP",
+				["point"] = "TOP",
+				["x"] = -435.9235229492188,
+			},
+		},
